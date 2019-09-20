25324b3524cfa2478a3eb0895a02c896928082bd
LihayMorad <lihaymorad@gmail.com>
2019-09-20 20:17:10 +0300

    able user to choose board size make clickable checkers have cursor pointer and background color make clicked checkers will have appropriate style (if it's moveable) add an option to capture opponent's checker add more moving rules and show an error accordingly change website font

--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -5,33 +5,36 @@ import './Square.css';
 class Square extends Component {
 
     componentDidMount() {
-        const { row, col, player, value } = this.props;
-        this.setState({ row, col, player, value });
+        const { row, col, player } = this.props;
+        this.setState({ row, col, player });
     }
 
     componentDidUpdate(prevProps) { // to make sure each dot has the latest props updated in its state
         if (prevProps.player !== this.props.player) { // prevent unnecessary renders
-            const { row, col, player, value } = this.props;
-            this.setState({ row, col, player, value });
+            const { row, col, player } = this.props;
+            this.setState({ row, col, player });
         }
     }
 
     onSquareClick = () => {
-        this.props.onSquareClick({ ...this.state });
+        if (this.props.player || this.props.checkerMove) {
+            this.props.onSquareClick({ ...this.state });
+        }
     }
 
     render() {
-        const { color, player } = this.props;
+        const { color, player, movingChecker } = this.props;
+        const isPlayerChecker = player && color === "navajowhite";
 
         return (
             <td
-                className="square"
+                className={`square ${isPlayerChecker && 'moveable'} ${movingChecker && 'movingChecker'}`}
                 style={{ backgroundColor: color }}
                 onClick={this.onSquareClick}
             >
                 {
-                    player && color === "navajowhite" && <span
-                        className="playerCircle"
+                    isPlayerChecker && <span
+                        className="playerChecker"
                         style={{ backgroundColor: player }}>
                     </span>
                 }
--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -4,79 +4,89 @@ import Square from '../../components/Square/Square';
 
 import './Board.css';
 
-const ROWS = 8;
-const COLS = 8;
-
 class Board extends Component {
 
     state = {
+        boardRows: 8,
+        boardCols: 8,
         checkerMove: false,
+        status: "init",
         prevBoard: []
     }
 
-    componentDidMount() {
-        this.initBoard();
-    }
+    updateBoardSizeValues = event => { this.setState({ [event.target.name]: event.target.value }) }
 
     initBoard = () => {
-        const board = new Array(ROWS);
-        for (let i = 0; i < ROWS; i++) {
-            board[i] = new Array(COLS);
-            for (let j = 0; j < COLS; j++) {
+        const { boardRows, boardCols } = this.state;
+        const board = new Array(boardRows);
+        for (let i = 0; i < boardRows; i++) {
+            board[i] = new Array(boardCols);
+            for (let j = 0; j < boardCols; j++) {
                 board[i][j] = {};
+                const isBlackSquare = (i + j) % 2 === 0;
                 let player = "";
-                if (i <= 2)
+                if (i <= 2 && !isBlackSquare)
                     player = "white";
-                else if (i >= 5)
+                else if (i >= boardRows - 3 && !isBlackSquare)
                     player = "red";
                 board[i][j].player = player;
                 board[i][j].key = `${i}_${j}`;
                 board[i][j].row = i;
                 board[i][j].col = j;
-                board[i][j].value = i + j;
-                board[i][j].color = (i + j) % 2 === 0 ? "black" : "navajowhite";
+                board[i][j].color = isBlackSquare ? "black" : "navajowhite";
+                board[i][j].movingChecker = false;
             }
         }
-        this.setState({ board });
+        this.setState({ board, status: "init" });
     }
 
     onSquareClick = square => {
+
         if (this.state.checkerMove) {
             const oldSquare = this.state.squareToMove;
             const newSquare = square;
             const prevBoard = JSON.parse(JSON.stringify(this.state.board)); // short way to clone nested object/array (there are more efficient ways)
             const updatedBoard = [...this.state.board];
 
+            updatedBoard[oldSquare.row][oldSquare.col].movingChecker = false;
             const move = this.validMove(oldSquare, newSquare);
             if (!move.legal) {
-                this.setState({ checkerMove: false, squareToMove: null });
+                this.setState({ board: updatedBoard, checkerMove: false, squareToMove: null });
                 alert("Illegal move: " + move.message);
                 return;
+            } else {
+                // change old square
+                updatedBoard[oldSquare.row][oldSquare.col].player = "";
+
+                // set new square
+                updatedBoard[newSquare.row][newSquare.col].player = oldSquare.player;
+
+                // capture opponent square
+                if (move.message === "capture") {
+                    updatedBoard[move.squareToCapture.row][move.squareToCapture.col].player = "";
+                }
+
+                this.setState({ checkerMove: false, squareToMove: null, board: updatedBoard, prevBoard, status: "playing" });
             }
-
-            updatedBoard[oldSquare.row][oldSquare.col].player = "";                 // change old square
-            updatedBoard[newSquare.row][newSquare.col].player = oldSquare.player;   // set new square
-
-            this.setState({ checkerMove: false, squareToMove: null, board: updatedBoard, prevBoard });
         }
 
         else {
-            this.setState({ checkerMove: true, squareToMove: square });
+            square.movingChecker = true;
+            const updatedBoard = [...this.state.board];
+            updatedBoard[square.row][square.col].movingChecker = true;
+            this.setState({ board: updatedBoard, checkerMove: true, squareToMove: square });
         }
 
     }
 
     validMove = (oldSquare, newSquare) => {
+
         if ((newSquare.row + newSquare.col) % 2 === 0) {
             return { legal: false, message: "You can't move to a black square!" };
         } else if (oldSquare.row === newSquare.row && oldSquare.col === newSquare.col) {
             return { legal: false, message: "You choose the same square!" };
-        } else if (Math.abs(oldSquare.col - newSquare.col) > 1) {
-            return { legal: false, message: "You can jump only one column ahead!" };
-        } else if (Math.abs(oldSquare.row - newSquare.row) > 1) {
-            return { legal: false, message: "You can jump only one row ahead!" };
         } else if (oldSquare.player === newSquare.player) {
-            return { legal: false, message: "You can't jump to square which is already occupied!" };
+            return { legal: false, message: "You can't jump to square which already occupied by you!" };
         }
 
         if (oldSquare.player === "white") {
@@ -88,27 +98,81 @@ class Board extends Component {
                 return { legal: false, message: "You can't move backwards!" };
             }
         }
-        return { legal: true, message: "" };
+
+        const capture = this.captureSquare(oldSquare, newSquare);
+        if (capture.able === true) {
+            return { legal: true, message: "capture", squareToCapture: capture.squareToCapture };
+        } else if (Math.abs(oldSquare.col - newSquare.col) > 1) {
+            return { legal: false, message: "You can jump only one column ahead!" };
+        } else if (Math.abs(oldSquare.row - newSquare.row) > 1) {
+            return { legal: false, message: "You can jump only one row ahead!" };
+        } else if (newSquare.player) {
+            return { legal: false, message: "You can't jump to square which already occupied by the opponent!" };
+        }
+
+        return { legal: true, message: "move" };
     }
 
-    undoOneMove = () => {
-        this.setState(state => ({ board: state.prevBoard, prevBoard: state.board }));
+    captureSquare = (oldSquare, newSquare) => {
+        const { board } = this.state;
+
+        if (Math.abs(oldSquare.row - newSquare.row) === 2 &&
+            Math.abs(oldSquare.col - newSquare.col) === 2) { // if player wants to capture
+
+            if (oldSquare.player === "white") {
+
+                if (board[oldSquare.row + 1][oldSquare.col + 1] &&
+                    board[oldSquare.row + 1][oldSquare.col + 1].player === "red" &&
+                    newSquare.player === "") {
+                    return { able: true, squareToCapture: { row: oldSquare.row + 1, col: oldSquare.col + 1 } };
+                }
+                if (board[oldSquare.row + 1][oldSquare.col - 1] &&
+                    board[oldSquare.row + 1][oldSquare.col - 1].player === "red" &&
+                    newSquare.player === "") {
+                    return { able: true, squareToCapture: { row: oldSquare.row + 1, col: oldSquare.col - 1 } };
+                }
+
+            }
+
+            else if (oldSquare.player === "red") {
+
+                if (board[oldSquare.row - 1][oldSquare.col + 1] &&
+                    board[oldSquare.row - 1][oldSquare.col + 1].player === "white" &&
+                    newSquare.player === "") {
+                    return { able: true, squareToCapture: { row: oldSquare.row - 1, col: oldSquare.col + 1 } };
+                }
+                if (board[oldSquare.row - 1][oldSquare.col - 1] &&
+                    board[oldSquare.row - 1][oldSquare.col - 1].player === "white" &&
+                    newSquare.player === "") {
+                    return { able: true, squareToCapture: { row: oldSquare.row - 1, col: oldSquare.col - 1 } };
+                }
+
+            }
+
+        }
+
+        return { able: false };
     }
+
+    undoOneMove = () => { this.setState(state => ({ board: state.prevBoard, prevBoard: state.board })); }
 
     render() {
-        const { board, prevBoard } = this.state;
+        const { boardRows, boardCols, board, status, checkerMove } = this.state;
 
         return (
             <>
-                <h3>THIS IS THE BOARD</h3>
+
+                <div id="boardSizeInputs">
+                    <input type="text" name="boardRows" value={boardRows} onChange={this.updateBoardSizeValues} />
+                    <input type="text" name="boardCols" value={boardCols} onChange={this.updateBoardSizeValues} />
+                    <button onClick={this.initBoard}>Apply</button>
+                </div>
 
                 <button onClick={this.initBoard}>Start / Reset Game</button>
                 {
-                    prevBoard.length > 0 && board != prevBoard && <div>
-                        <button onClick={this.undoOneMove}>
-                            Undo one move
+                    status === "playing" && <button onClick={this.undoOneMove}>
+                        Undo / Redo
                         </button>
-                    </div>
                 }
 
                 {
@@ -123,6 +187,7 @@ class Board extends Component {
                                                     return <Square
                                                         {...col}
                                                         className="col"
+                                                        checkerMove={checkerMove}
                                                         onSquareClick={this.onSquareClick}
                                                     />
                                                 })
